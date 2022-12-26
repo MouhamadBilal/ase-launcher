1db58687e9a5da8a8f6d061c429b898889158337
unknown <mb.salman95140@gmail.com>
2022-12-26 10:45:57 +0100

    build: Changing to oop configuration

--- pong.py
+++ pong.py
@@ -1,29 +1,37 @@
 import turtle
 
-#screen creation
+class Paddle(turtle.Turtle):
+    def __init__(self, x, y):
+        super().__init__()
+        self.speed(0)
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(x, y)
+    
+    def move_up(self):
+        y = self.ycor()
+        y += 20
+        self.sety(y)
+    
+    def move_down(self):
+        y = self.ycor()
+        y -= 20
+        self.sety(y)
 
-screen = turtle.Screen()
-screen.tiltle("Pong")
-screen.bgcolor("black")
-screen.setup(width=800, height=600)
-screen.tracer(0)
-
-# Pad a
-
-pad_a = turtle.Turtle()
-pad_a.speed(0)
-pad_a.shape("square")
-pad_a.color("white")
-pad_a.shapesize(stretch_wid=5, stretch_len=1)
-pad_a.penup()
-pad_a.goto(-350,0)
-
-# Pad_b
-pad_b = turtle.Turtle()
-pad_b.speed(0)
-pad_b.shape("square")
-pad_b.color("white")
-pad_b.shapesize(stretch_wid=5, stretch_len=1)
-pad_b.penup()
-pad_b.goto(350,0)
+class Ball(turtle.Turtle):
+    def __init__(self, x, y):
+        super().__init__()
+        self.speed(0)
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.goto(x, y)
+        self.dx = 0.1
+        self.dy = 0.1
+    
+    def move(self):
+        self.setx(self.xcor() + self.dx)
+        self.sety(self.ycor() + self.dy)
 
