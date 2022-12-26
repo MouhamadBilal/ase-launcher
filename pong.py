8006900464d8dff153d87604995c850d8bea5702
unknown <mb.salman95140@gmail.com>
2022-12-26 10:26:35 +0100

    feat: Main screen creation

---
+++ pong.py
@@ -0,0 +1,12 @@
+import turtle
+
+screen = turtle.Screen()
+screen.tiltle("Pong")
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
+#game loop
+
+while True:
+    screen.update()
