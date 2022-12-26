dc5652d7233b1cfee2f37e49427c6169d473b557
unknown <mb.salman95140@gmail.com>
2022-12-26 10:39:56 +0100

    feat: players pad creation

--- pong.py
+++ pong.py
@@ -1,12 +1,29 @@
 import turtle
 
+#screen creation
+
 screen = turtle.Screen()
 screen.tiltle("Pong")
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
-#game loop
+# Pad a
 
-while True:
-    screen.update()
+pad_a = turtle.Turtle()
+pad_a.speed(0)
+pad_a.shape("square")
+pad_a.color("white")
+pad_a.shapesize(stretch_wid=5, stretch_len=1)
+pad_a.penup()
+pad_a.goto(-350,0)
+
+# Pad_b
+pad_b = turtle.Turtle()
+pad_b.speed(0)
+pad_b.shape("square")
+pad_b.color("white")
+pad_b.shapesize(stretch_wid=5, stretch_len=1)
+pad_b.penup()
+pad_b.goto(350,0)
+
