11b5414ec5dc2bc6644b2db9fbedacc87fad33bf
unknown <mb.salman95140@gmail.com>
2023-01-05 15:57:50 +0100

    fix: Fixing some bugs (wip)

--- pong.py
+++ pong.py
@@ -35,3 +35,49 @@ class Ball(turtle.Turtle):
         self.setx(self.xcor() + self.dx)
         self.sety(self.ycor() + self.dy)
 
+# Set up the screen
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Create the paddles and ball
+paddle_a = Paddle(-350, 0)
+paddle_b = Paddle(350, 0)
+ball = Ball(0, 0)
+
+# Pen to write scores
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(paddle_a.move_up, "w")
+wn.onkeypress(paddle_a.move_down, "s")
+wn.onkeypress(paddle_b.move_up, "Up")
+wn.onkeypress(paddle_b.move_down, "Down")
+
+# Main game loop
+while True:
+    wn.update()
+    
+    # Move the ball
+    ball.move()
+    
+    # Check for ball collisions with top and bottom walls
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+    elif ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+    
+    # Check for ball collision with right paddle
+    if ball.xcor() > 340 and ball.xcor() < 350 and (paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
